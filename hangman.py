730480a1c180b66f13eb346d7109e6e09abaf953
Katka Marcincakova <katulik137@gmail.com>
2024-06-12 16:15:14 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,79 +1,60 @@
 import random
 
-def is_word_guessed(secret, guessed):
-    # ak su slova zo vstupu rovnake return True
-    # inak return False
-    if (secret == guessed):
-        return True
-    else:
-        return False
-
-def update_guessed(l, guessed, secret):
-    # doplni nove pismeno do uhadnuteho
-    result = ""
-    for i in range(len(guessed)):
-        if (secret[i] == l):
-            result += l
-        else:
-            result += guessed[i]
-    return result
-
-def get_available_letters(alphabet, letter):
-    # vyhodi guessed_letter z alphabet a vrati novy abecedu
-    result = ""
-    for a in alphabet:
-        if (a != letter):
-            result += a
-
-    return result
-
-def is_in_word(secret, letter):
-    if (letter in secret):
-        return True
-    else:
-        return False
-    
-def print_formatted(word):
-    result = ' '.join(word)
-    print(result)
-
-def get_random_word():
-    words = [ 'hollow', 'frequent', 'poke', 'tongue', 'quick', 'market', 'butter']
-    pos = random.randint(0, len(words) - 1)
+def random_word():
+    words = ["pes", "macka", "kon", "sliepka", "ovca"]
+    pos = random.randint(0, 4)
     return words[pos]
 
+def print_formatted(word):
+    print(' '.join(word))
 
-#samotny program
-secret = get_random_word()
-guessed = len(secret) * '_'
+secret = random_word()
 g_left = 8
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-print(secret)
-print('Welcome to the game, Hangman!')
+used_letters = []
+guessed = '_' * len(secret)
+#print_formatted(guessed)
+import random
+
+def random_word():
+    words = ["pes", "macka", "kon", "sliepka", "ovca"]
+    pos = random.randint(0, 4)
+    return words[pos]
+
+def print_formatted(word):
+    print(' '.join(word))
+
+secret = random_word()
+g_left = 8
+used_letters = []
+guessed = '_' * len(secret)
+#print_formatted(guessed)
+
+print('Welcome to the game Hangman!')
 print(f'I am thinking of a word that is {len(secret)} letters long.')
-print('-------------')
+print("----------")
 
-while (is_word_guessed(secret, guessed) == False):
-    print('Available letters:', end=' ')
-    print_formatted(alphabet)
-    letter = input('Please guess a letter: ')
+while (g_left != 0 and secret != guessed):
+    print(f'You have {g_left} left.')
+    print('Used letters:', end=' ')
+    print_formatted(used_letters)
+    letter = input("Please guess a letter: ")
 
-    if (len(letter) != 1):
-        print("Oops! That's not a letter:", end=' ')
-    elif (is_in_word(alphabet, letter) == False):
-        print("Oops! That's not a letter:", end=' ')
-    elif (is_in_word(secret, letter) == False):
-        print('Oops! That letter is not in my word:', end=' ')
-    else:
-        print('Good guess:', end=' ')
-        guessed = update_guessed(letter, guessed, secret)
+print('Welcome to the game Hangman!')
+print(f'I am thinking of a word that is {len(secret)} letters long.')
+print("----------")
 
-    alphabet = get_available_letters(alphabet, letter)
-    print_formatted(guessed)
-    print('-------------')
+while (g_left != 0 and secret != guessed):
+    print(f'You have {g_left} left.')
+    print('Used letters:', end=' ')
+    print_formatted(used_letters)
+    letter = input("Please guess a letter: ")
 
 
-print("Congratulations, you won!")
+
+
+
+
+
 
 
 
