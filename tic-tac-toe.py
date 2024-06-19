db2d24ba1dc824ddb08031bea5dbf4bde298a344
Katka Marcincakova <katulik137@gmail.com>
2024-06-19 17:00:25 +0200

    Add files via upload

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,26 +1,48 @@
 # - Funkcia inicializuje prázdne herné pole
 def initialize_board():
-    return [[' . ' for _ in range(4)] for _ in range(4)]
+    return [['.' for _ in range(4)] for _ in range(4)]
 
 # - Funkcia vykreslí aktuálny stav herného poľa na štandartný výstup
 def print_board(board):
     for row in board:
         print('|'.join(row))
 
-    print("-" * (len(board[0]) * 4 - 1))
-    print(" " + "   ".join(map(str, range(1, len(board[0]) + 1))))
+    print("-" * (len(board[0]) * 2 - 1))
+    print(" ".join(map(str, range(1, len(board[0]) + 1))))
 
 # - Funkcia položí krúžok hráča na do daného sĺpca
-def drop_piece(board, col, player_char):
-    return
+def drop_piece(col, player_char):
+    for i in range(3, -1, -1):
+        if board[i][col] == '.':
+            board[i][col] = player_char
+            return True
+
+    return False
 
 #- Funkcia overí vyhru v každom zo smerov
 def check_win(board):
     return
 
-# - Funkcia zistí či je herné pole už plné a teda jední sa o remízu
-def is_board_full(board):
-    return
+# - Funkcia zistí či je herné pole už plné a teda jedna sa o remízu
+def is_board_full():
+    for row in board:
+        if '.' in row:
+            return False
+    return True
 
+def get_current_char(x):
+    if x:
+        return 'X'
+    return 'O'
+
+x = True
 board = initialize_board()
 print_board(board)
+drop_piece(0, 'x')
+print_board(board)
+print(is_board_full())
+print(get_current_char(x))
+x = not x
+print(get_current_char(x))
+x = not x
+print(get_current_char(x))
