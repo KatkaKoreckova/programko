3e92fba5816473341ff10654d039a4fc787c9b0a
Katka Marcincakova <katulik137@gmail.com>
2024-06-26 17:00:12 +0200

    done

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,5 @@
+# https://github.com/KatkaMarcincakova/programko/
+
 # - Funkcia inicializuje prázdne herné pole
 def initialize_board():
     return [['.' for _ in range(4)] for _ in range(4)]
@@ -9,7 +11,7 @@ def print_board(board):
 
     print("-" * (len(board[0]) * 2 - 1))
     print(" ".join(map(str, range(0, len(board[0])))))
-    print("\n =================== \n")
+    print("\n===================\n")
 
 # - Funkcia položí krúžok hráča na do daného sĺpca
 def drop_piece(col, player_char):
@@ -22,6 +24,23 @@ def drop_piece(col, player_char):
 
 #- Funkcia overí vyhru v každom zo smerov
 def check_win(board):
+    # diagonaly
+    if board[0][0] == board[1][1] == board[2][2] == board[3][3] != '.':
+        return True
+
+    if board[0][3] == board[1][2] == board[2][1] == board[3][0] != '.':
+        return True
+
+    # riadky
+    for i in range(4):
+        if board[i][0] == board[i][1] == board[i][2] == board[i][3] != '.':
+            return True
+
+    # stlpce
+    for i in range(4):
+        if board[0][i] == board[1][i] == board[2][i] == board[3][i] != '.':
+            return True
+        
     return False
 
 # - Funkcia zistí či je herné pole už plné a teda jedna sa o remízu
@@ -39,3 +58,16 @@ def get_current_char(x):
 x = True
 board = initialize_board()
 print_board(board)
+
+while (is_board_full() == False and check_win(board) == False):
+    input_col = int(input('Zadaj stlpec: '))
+    if drop_piece(input_col, get_current_char(x)) == True:
+        print_board(board)
+        x = not x
+    else:
+        print('Plny stlpec. Vyber iny')
+
+if is_board_full():
+    print('Remiza')
+else:
+    print(f'Vyhral {get_current_char(not x)}')
